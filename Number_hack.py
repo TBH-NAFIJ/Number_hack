6285d914f62ba9e136a6977a24013b3995262cb1
TBH-NAFIJ <nafijsheikh880@gmail.com>
2024-10-13 19:02:30 +0600

    Add files via upload
    pkg update 
    pkg upgrade 
    pkg install python 
    pkg install git 
    git clone

---
+++ Number_hack.py
@@ -0,0 +1,31 @@
+import random
+def phonenumber(operator):
+ endNum = random.randint (10000000,99999999)
+ number = f"01{operator} {endNum}"
+ print(number)
+print('''CHOIS YOUR OPERATOR 
+        [1] GRAMENPHONE
+        [2] ROBI
+        [3] BAGALALINK
+	[4] AEIRTEL''')
+chois = input ('ENTER YOUR CHOIS:')
+amount =int(input('INTER YOUR CRACK LIMIT:')
+)
+if chois == '1':
+ for i in range (amount +1):
+   phonenumber(7)
+   
+elif chois == '2':
+ for i in range (amount +1):
+   phonenumber(8)
+
+if chois == '3':
+ for i in range (amount +1):
+   phonenumber(9)
+   
+if chois == '4':
+ for i in range (amount +1):
+   phonenumber(3)
+else:
+  print('wrong input')
+   
